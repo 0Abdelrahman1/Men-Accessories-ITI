1240ef9b40aa2e46c164561d08e608fe005475d7
Abdelrahman_ElWarraky <bodyelwarraky@gmail.com>
2026-05-09 20:31:58 +0300

    init

---
+++ Men Accessories/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Men_Accessories.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
