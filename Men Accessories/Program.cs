dc1b36ef977c18fa41a622beace1e37f81825296
Karim Shebl <karimshebl003@gmail.com>
2026-05-13 22:09:19 +0300

    Added the services to the container

--- Men Accessories/Program.cs
+++ Men Accessories/Program.cs
@@ -47,6 +47,9 @@ namespace Men_Accessories
 
             builder.Services.AddScoped<IProductRepository, ProductRepository>();
             builder.Services.AddScoped<IProductService, ProductService>();
+            builder.Services.AddScoped<ICartRepository, CartRepository>();
+            builder.Services.AddScoped<ICartService, CartService>();
+            builder.Services.AddScoped<IOrderRepository, OrderRepository>();
 
 
             builder.Configuration.AddJsonFile("appsettings.json", optional: false)
