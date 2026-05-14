45e5796366811bfb8a0e7e6abcc3bac1666b92ff
mohamedAbdelgawad-ux <mohamedai33.veo@gmail.com>
2026-05-15 02:11:03 +0300

    fix contact &  about page

--- Men Accessories/Utilities/EmailSettings.cs
+++ Men Accessories/Utilities/EmailSettings.cs
@@ -8,6 +8,7 @@ namespace Men_Accessories.Utilities
     {
         public static void SendEmail(Email email, IConfiguration configuration)
         {
+            // 1. قراءة البيانات بأمان من الإعدادات (من الـ HEAD)
             var smtpServer = configuration["EmailService:SmtpServer"];
             var port = int.Parse(configuration["EmailService:Port"]);
             var emailAddress = configuration["EmailService:Email"];
@@ -16,7 +17,19 @@ namespace Men_Accessories.Utilities
             var client = new SmtpClient(smtpServer, port);
             client.EnableSsl = true;
             client.Credentials = new NetworkCredential(emailAddress, password);
-            client.Send(emailAddress, email.To, email.Subject, email.Body);
+
+            // 2. تجهيز رسالة الإيميل (من الـ Incoming)
+            var message = new MailMessage();
+            message.From = new MailAddress(emailAddress); // استخدمنا الإيميل اللي جي من الـ Config
+            message.To.Add(email.To);
+            message.Subject = email.Subject;
+            message.Body = email.Body;
+            
+            // IMPORTANT
+            message.IsBodyHtml = true; 
+
+            // 3. الإرسال
+            client.Send(message);
         }
     }
 }
